4d09344d579d4ec3e4d2b2743f4d4a914ee6c56f
nihatozturktr <nihatozturktr@gmail.com>
2022-02-21 01:00:08 +0100

    pdracht 12 try

---
+++ src/x03_Circle/Circle.java
@@ -0,0 +1,29 @@
+package x03_Circle;
+
+public class Circle {
+
+    static double pi =3.14;
+    static double r;//radius
+    static double  c;//circumference
+    static double area;
+
+
+
+
+    public Circle(double pi, double r, double c){
+
+    }
+
+    static double Area(){
+        return area = pi*r*r;
+    }
+
+static double circumference(){
+
+        return c=2*pi*r;
+}
+
+
+
+
+}
---
+++ src/x03_Circle/CircleApp.java
@@ -0,0 +1,11 @@
+package x03_Circle;
+
+public class CircleApp {
+
+    public static void main(String[] args) {
+        //Circle Alan1 =new Circle(2.14,1.2,3.3);
+
+
+        //System.out.println(Alan1);
+    }
+}
