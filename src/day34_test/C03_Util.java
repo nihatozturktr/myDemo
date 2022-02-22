c5a3827c97624e8535cffcb8b2556b8671ad16cd
nihatozturktr <nihatozturktr@gmail.com>
2022-02-23 00:25:52 +0100

    get method practice

---
+++ src/day34_test/C03_Util.java
@@ -0,0 +1,16 @@
+package day34_test;
+
+public class C03_Util {
+
+
+    public static void main(String[] args) {
+
+
+        C04_SchoolApp student1 = new C04_SchoolApp(12,"Ali",Boolean.TRUE,Boolean.TRUE,300);
+
+
+        this.studentNane = studentNane;
+    }
+
+
+}
---
+++ src/day34_test/C04_SchoolApp.java
@@ -0,0 +1,45 @@
+package day34_test;
+
+public class C04_SchoolApp {
+
+    private  int studentID;
+
+    public int getStudentID() {
+        return studentID;
+    }
+
+    public Boolean getCallStatus() {
+        return callStatus;
+    }
+
+    public Boolean getResulStatus() {
+        return resulStatus;
+    }
+
+    public String getStudentNane() {
+        return studentNane;
+    }
+
+    public int getCoursePrice() {
+        return coursePrice;
+    }
+
+    private String studentNane;
+    private Boolean callStatus;
+    private Boolean resulStatus;
+    private  int coursePrice;
+
+    C04_SchoolApp(int studentID, String studentNane, Boolean callStatus, boolean resulStatus,
+                  int coursePrice){
+
+
+
+    }C04_SchoolApp(int studentID,Boolean callStatus){ // Ogrenci aranma durumu
+
+
+    }
+    }
+
+
+
+
