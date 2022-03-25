420bac6ae842a8469d645e6b8bda599768f70909
nihatozturktr <nihatozturktr@gmail.com>
2022-03-25 23:44:28 +0100

    Exception extra

--- src/hoofdstuk_16_Exception/extra/main.java
+++ src/hoofdstuk_16_Exception/extra/main.java
@@ -3,5 +3,14 @@ package hoofdstuk_16_Exception.extra;
 public class main {
     public static void main(String[] args) {
 
+
+
+        try{
+            int[] num = new int[]{1,2,3};
+        }catch(StringIndexOutOfBoundsException exception){
+
+
+        }
+
     }
 }
