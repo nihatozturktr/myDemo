a2b39be428b87a89ddae1225dc25cb5aa2765cce
nihatozturktr <nihatozturktr@gmail.com>
2022-04-04 15:47:50 +0200

    Pandemic Simulator-

---
+++ Pandemic Simulator/src/TemperatureSorter.java
@@ -0,0 +1,8 @@
+import java.util.Comparator;
+
+public class TemperatureSorter implements Comparator<Patient> {
+    @Override
+    public int compare(Patient o1, Patient o2) {
+        return 0;
+    }
+}
