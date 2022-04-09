e9c6b5f4147919df7e8793c1c8eacb8f3fb20ad8
nihatozturktr <nihatozturktr@gmail.com>
2022-04-10 01:29:58 +0200

    iterator

---
+++ src/day_44_iterator_collections/C01_Iterator.java
@@ -0,0 +1,27 @@
+package day_44_iterator_collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class C01_Iterator {
+
+    public static void main(String[] args) {
+        List<Integer> liste = new ArrayList<Integer>();
+        liste.add(5);
+        liste.add(3);
+        liste.add(7);
+        liste.add(1);
+        liste.add(9);
+        liste.add(8);
+        liste.add(2);
+
+        System.out.println(liste);
+
+
+    }
+
+
+
+
+
+}
