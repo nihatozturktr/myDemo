b8b00d6952e04938f1f0a7ec9349d1d1941647c9
nihatozturktr <nihatozturktr@gmail.com>
2022-04-11 02:30:21 +0200

    iterator2

---
+++ src/day_44_iterator_collections/C04_Iterator.java
@@ -0,0 +1,19 @@
+package day_44_iterator_collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class C04_Iterator {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+
+        list.add("Ali");
+        list.add("Veli");
+        list.add("Ayse");
+        list.add("Fatma");
+        list.add("Emrah");
+
+        System.out.println(list);//[Ali, Veli, Ayse, Fatma, Emrah]
+
+    }
+}
