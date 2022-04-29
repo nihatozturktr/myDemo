afd6363cc87466460d35f3da9ee768cf8dc4ce51
nihatozturktr <nihatozturktr@gmail.com>
2022-04-29 16:01:33 +0200

    Toets_Reading_Writing

---
+++ src/hoofdstuk_24/testReadingWriting/Pokemon.java
@@ -0,0 +1,30 @@
+package hoofdstuk_24.testReadingWriting;
+
+public class Pokemon {
+    private String name;
+    private boolean edible;
+
+
+
+    public Pokemon(String name, boolean edible) {
+        this.name = name;
+        this.edible = edible;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean getEdible() {
+        return edible;
+    }
+
+    public void setEdible(boolean edible) {
+        this.edible = edible;
+    }
+
+}
