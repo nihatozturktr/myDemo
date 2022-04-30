7e2977ab94188359c334f1755ee9f9ceadd274a4
nihatozturktr <nihatozturktr@gmail.com>
2022-05-01 00:23:02 +0200

    .

--- src/hoofdstuk_24/C04.java
+++ src/hoofdstuk_24/C04.java
@@ -1,11 +1,20 @@
 package hoofdstuk_24;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class C04 {
     public static void main(String[] args) {
+
+        File myFile = new File("//..//..//nihatozturk.txt");
+
+        if(!myFile.getParentFile().exists()){
+            myFile.getParentFile().mkdir();
+
+        }
+
 
 
 
